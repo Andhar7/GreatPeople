0c70db1bb7bc8c3c03bcd3dfaad3cf81e9fc1c23
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2020-12-17 19:10:06 +0100

    Initial Commit

---
+++ GreatPeople/GreatPeopleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreatPeopleApp.swift
+//  GreatPeople
+//
+//  Created by Andrej Kling on 17.12.20.
+//
+
+import SwiftUI
+
+@main
+struct GreatPeopleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
